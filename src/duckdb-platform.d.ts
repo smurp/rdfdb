67d6ce31cd6758ad38edf67860a5df19307e0a29
Shawn Murphy <smurp@smurp.com>
2024-11-28 11:32:37 -0800

    remove typescript

--- src/duckdb-platform.d.ts
+++
@@ -1,9 +0,0 @@
-// src/duckdb-platform.d.ts
-
-declare module 'duckdb-platform' {
-  export function getDuckDB(
-    location?: string,
-    dbMode?: number,
-    dbCallback?: (err: Error | null) => void
-  ): Promise<{ db: any; connection: any }>;
-}
